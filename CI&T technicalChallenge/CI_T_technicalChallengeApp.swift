76305c54dc69ab061937b4f2bb3b0d8e717b5ed1
Sergio Cesar <sergiocesar0203@gmail.com>
2025-09-24 18:09:02 -0400

    first commit

---
+++ CI&T technicalChallenge/CI_T_technicalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_T_technicalChallengeApp.swift
+//  CI&T technicalChallenge
+//
+//  Created by Sérgio César Lira Júnior on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CI_T_technicalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
